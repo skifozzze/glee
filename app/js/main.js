b54bd4768292f52aed7351b45224cb1b94912c9d
skifozzze <skifozzze@gmail.com>
2021-11-24 16:47:49 +0200

    back home

--- app/js/main.js
+++ app/js/main.js
@@ -19,4 +19,6 @@ $(function (){
 
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
+
+
 })
--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,8 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/lib/cjs/rateyo.js',
     'app/js/main.js'
   ])
     
